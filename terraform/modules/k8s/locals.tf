c1fc5c5bbceced38e3c18db60db42b3e9c070cea
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-31 22:50:38 +0200

    remove unqualified tags

--- terraform/modules/k8s/locals.tf
+++ terraform/modules/k8s/locals.tf
@@ -1,10 +1,7 @@
 locals {
   tags = {
     env         = "${var.environment}",
-    team        = "${var.team}",
-    owner       = "MGTheTrain",
     project     = "gitops-ftw",
-    app-purpose = "Installation of helm charts to an AKS",
     Stage       = "${var.environment}"
   }
 }
