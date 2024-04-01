9d7c676c1ff51bc66325cfffef7ecca3001ae9a5
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-01 17:50:18 +0200

    implement variables.tf and assign input variables in module scope

--- terraform/envs/sbx-k8s-configuration/main.tf
+++ terraform/envs/sbx-k8s-configuration/main.tf
@@ -1,4 +1,9 @@
 module "k8s" {
   source = "../../modules/k8s"
-  environment = "sbx"
+  environment = var.environment
+  k8s_namespace = var.k8s_namespace
+  acr_login_server_name = var.acr_login_server_name
+  acr_username = var.acr_username
+  acr_password = var.acr_password
+  gitops_tool = var.gitops_tool
 }
---
+++ terraform/envs/sbx-k8s-configuration/variables.tf
@@ -0,0 +1,35 @@
+variable "environment" {
+  default     = "sbx"
+  description = "The environment."
+  type        = string
+}
+
+# K8s
+variable "k8s_namespace" {
+  default     = "gitops-ftw"
+  description = "K8s gitops-ftw namespace."
+}
+
+variable "acr_login_server_name" {
+  default     = "TBD"
+  sensitive = true
+  description = "Sets an ACR registry server name."
+}
+
+variable "acr_username" {
+  default     = "TBD"
+  sensitive = true
+  description = "Sets an ACR user name."
+}
+
+variable "acr_password" {
+  default     = "TBD"
+  sensitive = true
+  description = "Sets an ACR password."
+}
+
+variable "gitops_tool" {
+  description = "String determining whether to install Argo CD or FluxCD. Viable options: [ argocd, fluxcd ]"
+  type        = string
+  default     = "argocd"
+}
