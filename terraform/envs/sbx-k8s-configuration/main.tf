a39a16c401de57a942a5045e537d252e1e062de0
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-02 20:53:33 +0200

    update hcl files utilizing the updated k8s module

--- terraform/envs/sbx-k8s-configuration/main.tf
+++ terraform/envs/sbx-k8s-configuration/main.tf
@@ -1,7 +1,6 @@
 module "k8s" {
   source                = "../../modules/k8s"
   environment           = var.environment
-  k8s_namespace         = var.k8s_namespace
   acr_login_server_name = var.acr_login_server_name
   acr_username          = var.acr_username
   acr_password          = var.acr_password
