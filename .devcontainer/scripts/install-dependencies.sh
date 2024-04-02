6c25178ce461e8ded4355a7b56658c3868f893c1
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-02 20:21:54 +0200

    install helm and kubectl

--- .devcontainer/scripts/install-dependencies.sh
+++ .devcontainer/scripts/install-dependencies.sh
@@ -12,3 +12,13 @@ curl -s https://fluxcd.io/install.sh | FLUX_VERSION=2.0.0 bash
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# helm
+https://get.helm.sh/helm-v3.14.3-linux-amd64.tar.gz
+tar -zxvf helm-v3.14.3-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
