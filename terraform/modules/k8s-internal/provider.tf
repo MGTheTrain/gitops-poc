a4c457c6f4ca1880a09787320f7ad296debfd144
Marvin Gajek <Marvin.Gajek1@web.de>
2025-01-26 13:55:11 +0100

    refactor the Legacy Modules to Accept Providers from the Caller

--- terraform/modules/k8s-internal/provider.tf
+++ terraform/modules/k8s-internal/provider.tf
@@ -1,18 +1,18 @@
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
 
-terraform {
-  required_providers {
-    helm = {
-      source  = "helm"
-      version = ">= 2.5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     helm = {
+#       source  = "helm"
+#       version = ">= 2.5.0"
+#     }
+#   }
+# }
